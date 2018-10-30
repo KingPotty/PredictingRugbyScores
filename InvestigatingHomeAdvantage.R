835b9f47485892ea0d8ee09bcf1b26fa946d7de0
KingPotty <38402557+KingPotty@users.noreply.github.com>
2018-10-30 11:57:53 +0000

    Update InvestigatingHomeAdvantage.R

--- InvestigatingHomeAdvantage.R
+++ InvestigatingHomeAdvantage.R
@@ -1,5 +1,5 @@
 #### SET UP ####
-setwd("C:/Users/tompo/OneDrive/Documents/Tom Potter/EMC 3")
+setwd("M:/EMS/EMC/EMC 3")
 
 Results1516 <- read.csv("Results_2015-16.csv")
 Results1617 <- read.csv("Results_2016-17.csv")
@@ -14,19 +14,27 @@ c <- length(which(Results1718$HomeScore > Results1718$AwayScore))/length(Results
 HomeAdv <- c()
 TotalWins <- c()
 
-for (team in length(unique(Results1516$HomeTeam))){
+for (team in unique(Results1516$HomeTeam)){
   HomeAdv <- append(HomeAdv, length(which(Results1516$HomeTeam == team & Results1516$HomeScore > Results1516$AwayScore)))
-  
-  
-  HomeAdv[team] <- HomeAdv[team] / (HomeAdv[team] + length(which(Results1718$AwayTeam == team & Results1718$AwayScore > Results1718$HomeScore)) + length(which(Results1617$AwayTeam == team & Results1617$AwayScore > Results1617$HomeScore)) + length(which(Results1516$AwayTeam == team & Results1516$AwayScore > Results1516$HomeScore)))
+  HomeAdv[length(HomeAdv)] <- HomeAdv[length(HomeAdv)] / (HomeAdv[length(HomeAdv)] + length(which(Results1516$AwayTeam == team & Results1516$AwayScore > Results1516$HomeScore)))
+  TotalWins <- append(TotalWins, length(which(Results1516$HomeTeam == team & Results1516$HomeScore > Results1516$AwayScore)) + length(which(Results1516$AwayTeam == team & Results1516$AwayScore > Results1516$HomeScore)))
 }
+
+for (team in unique(Results1617$HomeTeam)){
+  HomeAdv <- append(HomeAdv, length(which(Results1617$HomeTeam == team & Results1617$HomeScore > Results1617$AwayScore)))
+  HomeAdv[length(HomeAdv)] <- HomeAdv[length(HomeAdv)] / (HomeAdv[length(HomeAdv)] + length(which(Results1617$AwayTeam == team & Results1617$AwayScore > Results1617$HomeScore)))
+  TotalWins <- append(TotalWins, length(which(Results1617$HomeTeam == team & Results1617$HomeScore > Results1617$AwayScore)) + length(which(Results1617$AwayTeam == team & Results1617$AwayScore > Results1617$HomeScore)))
+}
+
+for (team in unique(Results1718$HomeTeam)){
+  HomeAdv <- append(HomeAdv, length(which(Results1718$HomeTeam == team & Results1718$HomeScore > Results1718$AwayScore)))
+  HomeAdv[length(HomeAdv)] <- HomeAdv[length(HomeAdv)] / (HomeAdv[length(HomeAdv)] + length(which(Results1718$AwayTeam == team & Results1718$AwayScore > Results1718$HomeScore)))
+  TotalWins <- append(TotalWins, length(which(Results1718$HomeTeam == team & Results1718$HomeScore > Results1718$AwayScore)) + length(which(Results1718$AwayTeam == team & Results1718$AwayScore > Results1718$HomeScore)))
+}
+
 
 print(HomeAdv)
-
-TotalWins <- c()
-for (team in names(HomeAdv)){
-    TotalWins <- append(TotalWins, length(which(Results1718$HomeTeam == team & Results1718$HomeScore > Results1718$AwayScore)) + length(which(Results1718$AwayTeam == team & Results1718$AwayScore > Results1718$HomeScore)))
-}
+print(TotalWins)
 
 plot(TotalWins, HomeAdv)
 
